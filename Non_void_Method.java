eb3718c87a7bfb056b3d51d140ec6c0f0d18523d
chaitud2006 <cc2214574@gmail.com>
2024-12-11 14:13:31 +0530

    Add files via upload

---
+++ Non_void_Method.java
@@ -0,0 +1,12 @@
+class Non_void_Method
+{
+    public static String s()
+    {
+        return("Hello Students");
+    }
+        public static void main(String args[])
+        {
+            String res=s();
+            System.out.println(res);
+        }
+}
