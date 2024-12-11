72a8c17745bfb82611f3f6b901f98e3e80033ef1
chaitud2006 <cc2214574@gmail.com>
2024-12-11 14:14:18 +0530

    Add files via upload

---
+++ Protected_Static.java
@@ -0,0 +1,11 @@
+class Protected_Static
+{
+    protected static void s()
+    {
+        System.out.println("Hello Students");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
